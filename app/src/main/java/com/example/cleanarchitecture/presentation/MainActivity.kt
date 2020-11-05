a55a4c98d6ecef862cbe392cd22b994b757738ef
rajeevranganathan <Puttin)(*051>
2020-11-05 23:10:28 +0530

    Set up navigation

--- app/src/main/java/com/example/cleanarchitecture/presentation/MainActivity.kt
+++ app/src/main/java/com/example/cleanarchitecture/presentation/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.cleanarchitecture.presentation
 
+import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
+import com.example.cleanarchitecture.R
 
 class MainActivity : FragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 }
